eea04df1ec6d00f2fce0ab081737f5976788c53c
Renan C. Lopes <renan.lopes@microsffer.com.br>
2018-08-15 14:13:04 -0300

    Acompanhamento do livro

---
+++ Mvc4InActionGuestBook/Mvc4InActionGuestBook/Controllers/GuestBookController.cs
@@ -0,0 +1,40 @@
+﻿using Mvc4InActionGuestBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc4InActionGuestBook.Controllers
+{
+    public class GuestBookController : Controller
+    {
+        private GuestbookContext _db = new GuestbookContext();
+        
+        public ActionResult Index()
+        {
+            var mostRecentEntries =
+                                    (from entry in _db.Entries
+                                     orderby entry.DateAdded descending
+                                     select entry).Take(20);
+
+            ViewBag.Entries = mostRecentEntries.ToList();
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(GuestbookEntry entry)
+        {
+            entry.DateAdded = DateTime.Now;
+
+            _db.Entries.Add(entry);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Mvc4InActionGuestBook/Mvc4InActionGuestBook/Models/GuestbookContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Mvc4InActionGuestBook.Models
+{
+    public class GuestbookContext : DbContext
+    {
+        public GuestbookContext() : base("Guestbook"){}
+        public DbSet<GuestbookEntry> Entries { get; set; }
+    }
+}
